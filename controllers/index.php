8c642219b4583f58d261bc4276b6c0f5d25e2db5
root <root@jenkins.(none)>
2015-12-18 03:32:04 +0000

    added tables to index

--- controllers/index.php
+++ controllers/index.php
@@ -3,30 +3,40 @@
 
 class index{
 
-
 	function __construct(){}
-
 	public function GET() {
-
-		
-   $name = 'Joe'; # user-supplied data
  
 try {
-    $conn = new PDO('mysql:host=localhost;dbname=employees', 'root', 'a7505489');
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
- 
-    $data = $conn->query('SELECT * FROM employees LIMIT 5');
- 
+echo "<link rel='stylesheet' href='/views/main.css' type='text/css'>
+<div class='idx'>
+<h2>Department Name</h2>
+</div>
+";
+
+	require 'config/sql.php'; //router
+
+    $data = $conn->query('SELECT * FROM departments');
+
+echo "<form action='demo_form.asp' method='get'>
+<select size='20' name='selectionField' style = 'width: 200px'";
+
     foreach($data as $row) {
-        print_r($row); 
-    }
+
+ echo" <option value='$row[dept_no]' >$row[dept_name]</option>
+
+";}
+echo "</select>   <input type='image' src='views/images/button.png' border='0' alt='Submit' style='
+    margin-left: 11px;
+    margin-bottom: 115px;
+    width: 147px;
+'/>
+";		
+		
 } catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
 }
 
 }
-
-
 
 }
 ?>
