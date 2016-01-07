dada50eb4ffc9206fb7f92cf826925c48c7aed1d
root <root@vultr.guest>
2016-01-07 21:54:31 +0000

    removed the Get route

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php
-ini_set('display_errors',1);
-error_reporting(E_ALL);
+//ini_set('display_errors',1);
+//error_reporting(E_ALL);
+
 require 'controllers/index.php'; //router
+require 'controllers/employees.php'; //router
+require 'controllers/departments.php'; //router
+require 'controllers/add.php'; //router
+
      $page_request = 'index';
      if(isset($_REQUEST['page'])) {
         $page_request = $_REQUEST['page'];
       }
       $page = new $page_request;
       $method = $_SERVER['REQUEST_METHOD'];
-      if($method != "GET"){
+     /* if($method != "GET"){
          echo $page->$method($_REQUEST['Fname'],$_REQUEST['Lname'],$_REQUEST['Email']);
       }
       else
-         echo $page->$method();
+         */echo $page->$method();
 ?>
